6b3d9d98118e6e32adbef66679a66537ae0551ae
Sarah Hinton <smcfarland2@gmail.com>
2021-05-09 22:59:11 +1000

    Beginning employee tests

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -6,3 +6,15 @@
 // can get id via getId()
 // can get email via getEmail()
 // getRole() should return "Employee" (1ms)
+
+const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+    it("can instantiate employee instance", () => {
+        const worker = new Employee("Sarah", 28, "someone@email.com");
+
+        expect(worker.name).toEqual("Sarah");
+        expect(worker.id).toEqual(28);
+        expect(worker.email).toEqual("someone@email.com");
+    });
+})
