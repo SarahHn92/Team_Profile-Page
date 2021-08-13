b3e5d84086e01434fc9bf8e0a21e12ffdb7a45d3
Sarah Hinton <smcfarland2@gmail.com>
2021-08-13 22:58:45 +1000

    required packages and classes in index.js, began inquirer question arrays

---
+++ index.js
@@ -0,0 +1,10 @@
+// Require packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+const jest = require('jest');
+
+
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern =  require('./lib/Intern');
