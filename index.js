ce4ca4ca972178fedcaae05076bb6f8542d54944
Sarah Hinton <smcfarland2@gmail.com>
2021-08-15 12:42:25 +1000

    completed engineer prompts

--- index.js
+++ index.js
@@ -11,5 +11,47 @@ const Engineer = require('./lib/Engineer');
 const Intern =  require('./lib/Intern');
 
 const managerRole = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "Manager's name:",
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Manager's ID (number):"
+    },
+    {
+        type: 'type',
+        name: 'email',
+        message: "Manager's email:"
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: "Manager's office number:"
+    }
+]
 
+const engineerRole = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "Engineer's name:",
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Engineer's ID (number):"
+    },
+    {
+        type: 'type',
+        name: 'email',
+        message: "Engineer's email:"
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: "Engineer's GitHub username:"
+    }
 ]
