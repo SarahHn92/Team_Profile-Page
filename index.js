8be993a9c75198daa890cbaa5df6f95c09695b26
Sarah Hinton <smcfarland2@gmail.com>
2021-08-15 12:50:52 +1000

    Intern prompts added, also initial role choices

--- index.js
+++ index.js
@@ -4,12 +4,30 @@ const fs = require('fs');
 const jest = require('jest');
 const html = require('./src/teamProfile.html');
 
-
+// Require classes
 const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern =  require('./lib/Intern');
 
+
+// List prompt for role choices
+const chooseRoles = [
+    {
+        type: 'list',
+        name: 'role',
+        message: "Choose team member's role:",
+        choices: [
+            'Manager',
+            'Engineer',
+            'Intern',
+            'Team build completed.'
+        ]
+    }
+]
+
+
+// Prompts for manager role
 const managerRole = [
     {
         type: 'input',
@@ -33,6 +51,7 @@ const managerRole = [
     }
 ]
 
+// Prompts for engineer role
 const engineerRole = [
     {
         type: 'input',
@@ -53,5 +72,29 @@ const engineerRole = [
         type: 'input',
         name: 'officeNumber',
         message: "Engineer's GitHub username:"
+    }
+]
+
+// Prompts for Intern role
+const internRole = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "Intern's name:",
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Intern's ID (number):"
+    },
+    {
+        type: 'type',
+        name: 'email',
+        message: "Intern's email:"
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: "Intern's school:"
     }
 ]
